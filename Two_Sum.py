e0046a1ee3640e42c9906726469d4cc78f688073
suryabbrj <suryababuraj02@gmail.com>
2023-05-11 03:14:12 +0530

    finished q1.two_sums, took about 15 mins to get through it!

---
+++ Two_Sum.py
@@ -0,0 +1,27 @@
+
+# Q -  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+#   You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+#   You can return the answer in any order.
+
+#   Only one valid answer exists.
+
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                total = nums[i] + nums[j]
+                if total == target:
+                    return[i,j]        
+
+
+
+# solved!!!
